64bdd16ed5b3a9eff6ea260c2992234d6dc47649
PEDRO <130096211+PedroP57xd@users.noreply.github.com>
2024-01-07 21:28:51 -0500

    parche login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -6,6 +6,7 @@ import {
   FormBuilder,
   Validators
 } from '@angular/forms'
+import { AlertController } from '@ionic/angular';
 
 
 
@@ -18,7 +19,9 @@ export class LoginPage implements OnInit {
 
   formularioLogin: FormGroup;
 
-  constructor(public fb: FormBuilder) {
+  constructor(public fb: FormBuilder,
+    public alertController: AlertController
+    ) {
 
     this.formularioLogin=this.fb.group({
       'correo' : new FormControl("",Validators.required),
@@ -28,5 +31,26 @@ export class LoginPage implements OnInit {
 
   ngOnInit() {
   }
+
+  async ingresar(){
+    var f = this.formularioLogin.value;
+    var usuario: string | null = localStorage.getItem('usuario');
+    if(usuario!==null){
+      var usuarioString: { correo: string; password: string } = JSON.parse(usuario);
+
+      if(usuarioString.correo == f.correo && usuarioString.password == f.password){
+        console.log('Ingresado');
+      }else{
+        const alert = await this.alertController.create({
+          header: 'Datos incorrectos',
+          message: 'Los datos que ingresaste son incorrectos.',
+          buttons: ['Aceptar']
+        });
+    
+        await alert.present();
+      }
+    }
+  }
+
 
 }
--- src/app/sign-up/sign-up.page.ts
+++ src/app/sign-up/sign-up.page.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validator,
+  FormBuilder,
+  Validators
+} from '@angular/forms'
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +15,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpPage implements OnInit {
 
-  constructor() { }
+  formularioRegistro: FormGroup;
+
+  constructor(public fb: FormBuilder,
+    public alertController: AlertController) { 
+    this.formularioRegistro=this.fb.group({
+      'nombre' : new FormControl("",Validators.required),
+      'correo' : new FormControl("",Validators.required),
+      'contraseña' : new FormControl("",Validators.required)
+    });
+
+  }
+  
 
   ngOnInit() {
+  }
+
+  async guardar(){
+    var f=this.formularioRegistro.value;
+
+    if (this.formularioRegistro.invalid) {
+      const alert = await this.alertController.create({
+        header: 'Datos incompletos',
+        message: 'Por Favor llenar todos los datos',
+        buttons: ['Aceptar']
+      });
+      await alert.present();
+      return;
+    }
+
+    var usuario = {
+      correo: f.correo,
+      contraseña: f.contraseña
+    }
+
+    localStorage.setItem('usuario',JSON.stringify(usuario));
   }
 
 }
