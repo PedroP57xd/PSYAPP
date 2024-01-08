7b06b7c4a46341320868b4dfc15bb6b93da3882e
PEDRO <130096211+PedroP57xd@users.noreply.github.com>
2024-01-07 21:36:11 -0500

    account

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,6 +23,10 @@ const routes: Routes = [
     path: 'sign-up',
     loadChildren: () => import('./sign-up/sign-up.module').then( m => m.SignUpPageModule)
   },
+  {
+    path: 'account',
+    loadChildren: () => import('./account/account.module').then( m => m.AccountPageModule)
+  },
 ];
 
 @NgModule({
